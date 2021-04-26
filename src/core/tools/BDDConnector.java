92d55c4ecfd9bf1f7c6c1b0abbaf1be228181f50
Loic DUMAY <dumay.famille@gmail.com>
2021-04-26 11:48:08 +0200

    v0.0.12 - Star-Wars - Part 5.2

--- src/core/tools/BDDConnector.java
+++ src/core/tools/BDDConnector.java
@@ -15,6 +15,22 @@ import java.sql.Statement;
  *
  * @author ldumay
  */
+
+/**
+ * Class - BDDConnector
+ <br>
+ * <br>Constructor :
+ * <br>- BDDConnector()
+ <br>
+ * <br>Functions :
+ * <br>- Console.datasInsert(data)
+ * <br> |--> void
+ * <br>
+ * <br>- Console.datasSelect()
+ * <br>  |--> void
+ * <br>
+ * <br>End.
+ */
 public class BDDConnector {
 
     private final String strClassName = "com.mysql.jdbc.Driver";
@@ -25,8 +41,23 @@ public class BDDConnector {
     private String bddPort = "3306";
     public String bddUrl = "jdbc:mysql://"+this.bddIP+":"+this.bddPort+"/"+this.bddName;
 
+    /**
+     * Constructor
+     * 
+     * BDDConnector()
+     */
     public BDDConnector() {}
 
+    /**
+     * Constructor
+     * 
+     * BDDConnector(String bddName, String bddLogin, String bddPassword, String bddIP, String bddPort)
+     * @param bddName
+     * @param bddLogin
+     * @param bddPassword
+     * @param bddIP
+     * @param bddPort
+     */
     public BDDConnector(String bddName, String bddLogin, String bddPassword, String bddIP, String bddPort) {
         this.bddName = bddName;
         this.bddLogin = bddLogin;
@@ -35,12 +66,18 @@ public class BDDConnector {
         this.bddPort = bddPort;
     }
 
-    public void datasInsert() throws SQLException {
+    /**
+     * Requète d'insertion dans une base de donnée
+     * 
+     * SystemPrint()
+     * @throws java.sql.SQLException
+     */
+    public void datasInsert(String sqlQuery) throws SQLException {
         try {
             Class.forName(this.strClassName);
             Connection conn = DriverManager.getConnection(this.bddUrl, this.bddLogin, this.bddPassword);
             Statement stAddUser = (Statement) conn.createStatement();
-            stAddUser.executeUpdate("insert into Acces values (5,'Tom','tomahawk','indien','Etudiant',22)");
+            stAddUser.executeUpdate(sqlQuery);
             conn.close();
         } catch (ClassNotFoundException e) {
             System.err.println("Driver non chargé !");
@@ -51,6 +88,11 @@ public class BDDConnector {
         }
     }
 
+    /**
+     * Requète de lecture dans une base de donnée
+     * 
+     * datasSelect()
+     */
     public void datasSelect() {
         try {
             Class.forName(this.strClassName);
